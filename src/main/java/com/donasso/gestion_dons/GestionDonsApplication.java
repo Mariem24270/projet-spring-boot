1d958f022c30c62c14092a7fe99f1a4cce85b163
Mariem <24270@supnum.mr>
2026-05-25 17:06:04 +0000

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/donasso/gestion_dons/GestionDonsApplication.java
@@ -0,0 +1,13 @@
+package com.donasso.gestion_dons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donasso/gestion_dons/GestionDonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donasso.gestion_dons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
